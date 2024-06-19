0a47ffc865924b5b404e9e32b609953bebce38f8
Kshitij <frontendwithkshitij@gmail.com>
2024-06-19 19:10:42 +0530

    add responsive css

--- accordian/js/script.js
+++ accordian/js/script.js
@@ -1,27 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var acc = document.getElementsByClassName("accordin");
-  let chevronIcon = document.querySelectorAll('.fa-chevron-right');
+// document.addEventListener('DOMContentLoaded', () => {
+//   var acc = document.querySelectorAll(".accordion");
+//   let chevronIcon = document.querySelectorAll('.fa-chevron-right');
 
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
+//   for (let i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//       this.classList.toggle("active");
 
-      chevronIcon.forEach((element) => {
+//       chevronIcon.forEach((element) => {
+//         if (element.parentElement && element.parentElement.parentElement && element.parentElement.parentElement.classList.contains('active')) {
+//           element.style.transform = "rotate(90deg)";
+//         } else {
+//           element.style.transform = "rotate(0deg)";
+//         }
+//       });
+
+//       var panel = this.nextElementSibling;
+//       if (panel) {
+//         if (panel.style.maxHeight) {
+//           panel.style.maxHeight = null;
+//          }else {
+//           panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//       }
+//     });
+//   }
+// });
+
+
+const  accordian = document.querySelectorAll(".accordion");
+let chevronIcon = document.querySelectorAll('.fa-chevron-right');
+accordian.forEach((element)=>{
+  element.addEventListener('click',function(){
+    this.classList.toggle('active')
+chevronIcon.forEach((element) => {
         if (element.parentElement && element.parentElement.parentElement && element.parentElement.parentElement.classList.contains('active')) {
           element.style.transform = "rotate(90deg)";
         } else {
           element.style.transform = "rotate(0deg)";
         }
       });
-
-      var panel = this.nextElementSibling;
-      if (panel) {
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      }
-    });
-  }
-});
+    let panel = this.nextElementSibling;
+    if(panel.style.maxHeight){
+      panel.style.maxHeight = null
+    }else{
+      panel.style.maxHeight = panel.scrollHeight  + "px"
+    }
+  })
+})
